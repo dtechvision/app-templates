9532481c1f03418261333ca3ca56285d8629873d
Samuel Huber <samu@smashice.de>
2025-02-17 00:35:45 +0100

    feat: add DATABASE_URL env variable support

--- webapp-remix-effect-pg/app/config.ts
+++ webapp-remix-effect-pg/app/config.ts
@@ -1,26 +1,42 @@
 import { PgClient } from "@effect/sql-pg";
-import { Config, Effect, Layer } from "effect";
+import { Config, Effect, Layer, Option } from "effect";
 
 export const SqlLive = Layer.unwrapEffect(
   Effect.gen(function* () {
-    const database = yield* Config.nonEmptyString("DATABASE_NAME").pipe(
-      Config.withDefault("effect_pg_dev"),
-    );
-    const username = yield* Config.nonEmptyString("DATABASE_USER").pipe(
-      Config.withDefault("postgres"),
-    );
-    const host = yield* Config.nonEmptyString("DATABASE_HOST").pipe(
-      Config.withDefault("localhost"),
-    );
-    const port = yield* Config.number("DATABASE_PORT").pipe(
-      Config.withDefault(5432),
+    const url = yield* Config.option(Config.redacted("DATABASE_URL")).pipe(
+      Effect.flatMap(Option.match({
+        // if DATABASE_URL is set, use it
+        onSome: (url) => Effect.gen(function* () {
+          yield* Effect.logInfo("[SqlLive] using DATABASE_URL");
+          return PgClient.layer({ url });
+        }),
+        // if DATABASE_URL is not set, use individual env variables
+        onNone: () => Effect.gen(function* () {
+          yield* Effect.logInfo("[SqlLive] no DATABASE_URL, using individual env variables");
+          const database = yield* Config.nonEmptyString("DATABASE_NAME").pipe(
+            Config.withDefault("effect_pg_dev"),
+          );
+          const username = yield* Config.nonEmptyString("DATABASE_USER").pipe(
+            Config.withDefault("postgres"),
+          );
+          const password = yield* Config.redacted("DATABASE_PASS");
+          const host = yield* Config.nonEmptyString("DATABASE_HOST").pipe(
+            Config.withDefault("localhost"),
+          );
+          const port = yield* Config.number("DATABASE_PORT").pipe(
+            Config.withDefault(5432),
+          );
+          return PgClient.layer({
+            database,
+            username,
+            password,
+            host,
+            port
+          });
+        }),
+      }))
     );
 
-    return PgClient.layer({
-      database,
-      username,
-      host,
-      port,
-    });
+    return url;
   }),
 );
